74832745efe671b8f81d824dc13ac9106087be56
zakaria abdin <zakariaabdin5@gmail.com>
2024-01-20 13:15:37 +0200

    turning to new project with new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import {BrowserRouter , Routes , Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+
+
 
 
 export default function App() {
   return (
     <BrowserRouter>
-     <Header/>
      <Routes>
-       {/* <Route path='/' element={<Home/>}/>
-       <Route path='/sign-in' element={<Signin/>}/>
-       <Route path='/sign-up' element={<Signup/>}/>
+       <Route path='/' element={<Home />}/>
        <Route path='/about' element={<About/>}/>
-       <Route path='/profile' element={<Profile/>}/> */}
+       <Route path='/sign-in' element={<Signin/>} />
+       <Route path='/sign-up' element={<Signup/>} />
+       <Route path='/dashboard' element={<Dashboard/>} />
+       <Route path='/projects' element={<Projects/>} />
      </Routes>
     </BrowserRouter>
   )
